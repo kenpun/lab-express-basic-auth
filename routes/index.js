c4c9b6f4bd82fd38a126d3e18ed171a8bb66d7d0
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-07 17:50:38 +0200

    TODO: add /mainroute and view

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
 function loginCheck() {
   return (req, res, next) => {
     // check if the user is logged in
@@ -15,19 +20,20 @@ function loginCheck() {
   }
 }
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
-
 router.get('/profile', loginCheck(), (req, res, next) => {
-  // whenever you want to use use info in a view
+  // whenever you want to use user info in a view
   // these are the steps
   // access the logged in user  
   const username = req.session.user.username
   res.render('profile', { username: username})
 });
 
+    // TODO: iteration 3 | Protected Routes
+router.get('/main', loginCheck(), (req, res, next) => {
+  const username = req.session.user.username
+  console.log('has been reloaded');
+  res.redirect('main', { username: username })
+})
 
 
 module.exports = router;
